16756c84d9fd81c3560010b168e9cdf3736914ff
WingSunCheng <wingsuncheng09@gmail.com>
2023-03-15 17:31:25 -0400

    Initial commit first intellij test

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.LinkedList;
+public class Main {
+
+            public static void main( String[] args ) {
+                LinkedList<String> list = new LinkedList<String>();
+                list.add( "Hello" );
+                list.add( "World" );
+                list.add( "!" );
+                String x = list.get( 1 );
+                System.out.println(x);
+            }
+
+
+        }
+
+
+
+
